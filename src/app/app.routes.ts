352bcee33d5db4391e8b3e19375833f9610e1887
María López <37983313+oppahero@users.noreply.github.com>
2024-05-17 14:58:57 -0400

    added: default page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { CountryListComponent } from './pages/country-list/country-list.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CountryListComponent
+  }
+]
