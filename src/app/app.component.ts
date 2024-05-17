6041ccf3d6b53c0e529dffbc97c2926bd1617b03
María López <37983313+oppahero@users.noreply.github.com>
2024-05-17 13:17:38 -0400

    added: Angular icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { NgIconComponent, provideIcons } from '@ng-icons/core'
+import { heroMagnifyingGlass, heroMoon } from '@ng-icons/heroicons/outline'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIconComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [provideIcons({ heroMoon, heroMagnifyingGlass })]
 })
 export class AppComponent {
-  title = 'country-search-page';
 }
