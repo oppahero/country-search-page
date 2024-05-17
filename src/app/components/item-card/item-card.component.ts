389f33f857c5818d8f16e65f6284e9a42002dc61
María López <37983313+oppahero@users.noreply.github.com>
2024-05-17 15:27:08 -0400

    added: data input and card structure

--- src/app/components/item-card/item-card.component.ts
+++ src/app/components/item-card/item-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core'
+import { Country } from '../../interfaces'
 
 @Component({
   selector: 'app-item-card',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './item-card.component.css'
 })
 export class ItemCardComponent {
-
+  @Input() country!: Country
 }
