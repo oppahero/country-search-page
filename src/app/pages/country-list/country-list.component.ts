80ca8ceee503657f6454b366745b50e9c5052dbd
María López <37983313+oppahero@users.noreply.github.com>
2024-05-17 15:27:50 -0400

    added: ItemCardComponent to imports

--- src/app/pages/country-list/country-list.component.ts
+++ src/app/pages/country-list/country-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
 import { heroMagnifyingGlass, heroMoon } from '@ng-icons/heroicons/outline'
 import { NgIconComponent, provideIcons } from '@ng-icons/core'
+import { ItemCardComponent } from '../../components/item-card/item-card.component'
 
 @Component({
   selector: 'app-country-list',
   standalone: true,
-  imports: [NgIconComponent],
+  imports: [NgIconComponent, ItemCardComponent],
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.css',
   providers: [provideIcons({ heroMoon, heroMagnifyingGlass })]
