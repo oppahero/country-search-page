ee8ea932400b2e3ecea551ce5050fd39f80abac9
María López <37983313+oppahero@users.noreply.github.com>
2024-05-20 13:16:53 -0400

    updated: consume service and show cards with country information

--- src/app/components/item-card/item-card.component.ts
+++ src/app/components/item-card/item-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core'
 import { Country } from '../../interfaces'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-item-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './item-card.component.html',
   styleUrl: './item-card.component.css'
 })
--- src/app/pages/country-list/country-list.component.ts
+++ src/app/pages/country-list/country-list.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core'
+import { Component, OnInit, inject } from '@angular/core'
 import { heroMagnifyingGlass, heroMoon } from '@ng-icons/heroicons/outline'
 import { NgIconComponent, provideIcons } from '@ng-icons/core'
 import { ItemCardComponent } from '../../components/item-card/item-card.component'
+import { CountriesService } from '../../services/countries.service'
+import { Country } from '../../interfaces'
 
 @Component({
   selector: 'app-country-list',
@@ -11,6 +13,25 @@ import { ItemCardComponent } from '../../components/item-card/item-card.componen
   styleUrl: './country-list.component.css',
   providers: [provideIcons({ heroMoon, heroMagnifyingGlass })]
 })
-export class CountryListComponent {
+export class CountryListComponent implements OnInit {
+  countriesService: CountriesService = inject(CountriesService)
+  countries!: Country[]
 
+  ngOnInit (): void {
+    this.countriesService.getAllCountries()
+      .subscribe(
+        {
+          next: (v) => { this.countries = v; console.log(v) },
+          error: (e) => console.error(e)
+        }
+      )
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  async getWithFetchApi () {
+    await this.countriesService.getCountries()
+      .then((res: Country[]) => {
+        this.countries = res
+      })
+  }
 }
