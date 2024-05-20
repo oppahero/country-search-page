c1e624b081fb80880a30faef29f697286bf83909
María López <37983313+oppahero@users.noreply.github.com>
2024-05-20 13:12:55 -0400

    added: countries service and HttpClient

---
+++ src/app/services/countries.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core'
+import { Country } from '../interfaces'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+// import * as data from './db.json'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+  private readonly url = 'http://localhost:3000/data'
+
+  constructor (private readonly http: HttpClient) {}
+
+  // Con Fetch Api
+  async getCountries (): Promise<Country[]> {
+    const data = await fetch(this.url)
+    return (await data.json()) ?? []
+  }
+
+  getAllCountries (): Observable<Country[]> {
+    return this.http.get<Country[]>(this.url)
+  }
+}
