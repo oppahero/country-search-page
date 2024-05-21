0b5989ffe3234286b48fad54da650f671fad0063
María López <37983313+oppahero@users.noreply.github.com>
2024-05-21 13:27:20 -0400

    added: country detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router'
 import { CountryListComponent } from './pages/country-list/country-list.component'
+import { CountryDetailComponent } from './pages/country-detail/country-detail.component'
 
 export const routes: Routes = [
   {
     path: '',
     component: CountryListComponent
+  },
+  {
+    path: ':country',
+    component: CountryDetailComponent
   }
 ]
