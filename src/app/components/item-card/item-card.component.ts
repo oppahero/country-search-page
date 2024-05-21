0baa3aedcc6f03a9e8a54581201be950e784c10b
María López <37983313+oppahero@users.noreply.github.com>
2024-05-21 13:43:51 -0400

    added: card routerlink

--- src/app/components/item-card/item-card.component.ts
+++ src/app/components/item-card/item-card.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core'
 import { Country } from '../../interfaces'
 import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-item-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './item-card.component.html',
   styleUrl: './item-card.component.css'
 })
 export class ItemCardComponent {
   @Input() country!: Country
+
+  getId (): string {
+    return this.country.name.toLowerCase().replace(/ /g, '-')
+  }
 }
