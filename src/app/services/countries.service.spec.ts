f5b70f19318a56fa3e672030f61388c50e3a38e4
María López <37983313+oppahero@users.noreply.github.com>
2024-09-11 13:03:23 -0400

    updated: country service

--- src/app/services/countries.service.spec.ts
+++ src/app/services/countries.service.spec.ts
@@ -1,16 +1,37 @@
-import { TestBed } from '@angular/core/testing'
-
 import { CountriesService } from './countries.service'
+import { HttpClient } from '@angular/common/http'
+import { asyncData } from '../testing/async-observable-helpers'
+
+import mockedData from '../../../db.json'
 
 describe('CountriesService', () => {
   let service: CountriesService
+  let httpClientSpy: jasmine.SpyObj<HttpClient>
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
-    service = TestBed.inject(CountriesService)
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
+    service = new CountriesService(httpClientSpy)
   })
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('url should be in localhost:3000', () => {
+    expect(service.url).toEqual('http://localhost:3000/data')
+  })
+
+  it('getAllCountries: should return the expected list of Countries (successful)', (done: DoneFn) => {
+    httpClientSpy.get.and.returnValue(asyncData(mockedData))
+
+    service.getAllCountries().subscribe({
+      next: (res: any) => {
+        expect(res.data)
+          .withContext('expected data')
+          .toEqual(mockedData.data)
+        done()
+      },
+      error: done.fail
+    })
   })
 })
--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -8,15 +8,15 @@ import { Observable } from 'rxjs'
   providedIn: 'root'
 })
 export class CountriesService {
-  private readonly url = 'http://localhost:3000/data'
+  readonly url = 'http://localhost:3000/data'
 
   constructor (private readonly http: HttpClient) {}
 
   // Con Fetch Api
-  async getCountries (): Promise<Country[]> {
-    const data = await fetch(this.url)
-    return (await data.json()) ?? []
-  }
+  // async getCountries (): Promise<Country[]> {
+  //   const data = await fetch(this.url)
+  //   return (await data.json()) ?? []
+  // }
 
   getAllCountries (): Observable<Country[]> {
     return this.http.get<Country[]>(this.url)
