70b006266660ac54269aa58ceaa7594b79f4d4c9
María López <37983313+oppahero@users.noreply.github.com>
2024-09-12 15:11:17 -0400

    updated: countryListComponent test

--- src/app/pages/country-list/country-list.component.spec.ts
+++ src/app/pages/country-list/country-list.component.spec.ts
@@ -1,23 +1,72 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { CountryListComponent } from './country-list.component'
+import { CountriesService } from '../../services/countries.service'
+import mockedData from '../../../../db.json'
+import { of } from 'rxjs'
+import {
+  FilterByComponent,
+  ItemCardComponent,
+  SearchInputComponent
+} from '../../components'
 
-describe('CountryListComponent', () => {
+fdescribe('CountryListComponent', () => {
   let component: CountryListComponent
   let fixture: ComponentFixture<CountryListComponent>
 
+  let countriesServiceSpy = jasmine.createSpyObj('CountriesService', [
+    'getAllCountries'
+  ])
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CountryListComponent]
-    })
-      .compileComponents()
+      imports: [
+        CountryListComponent,
+        ItemCardComponent,
+        SearchInputComponent,
+        FilterByComponent
+      ],
+      providers: [{ provide: CountriesService, useValue: countriesServiceSpy }]
+    }).compileComponents()
 
     fixture = TestBed.createComponent(CountryListComponent)
     component = fixture.componentInstance
+
+    countriesServiceSpy = TestBed.inject(CountriesService)
+
+    countriesServiceSpy.getAllCountries.and.returnValue(of(mockedData))
+
     fixture.detectChanges()
+
+    await fixture.whenStable()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('#consult should call getAllCountries', () => {
+    expect(countriesServiceSpy.getAllCountries).toHaveBeenCalled()
+    expect(component.filterCountries).toBeDefined()
+    expect(component.countries).toBeDefined()
+  })
+
+  it('#filterByCountry filter by searchInput text', () => {
+    component.filterCountries = mockedData.data
+
+    const expectedCountry = ['Belgium', 'Belize', 'Belarus']
+
+    component.filterByCountry('Bel')
+
+    expect(component.filterCountries.length).toBe(3)
+
+    const names = component.filterCountries.reduce<string[]>((accumulator, item) => {
+      accumulator.push(item.name)
+      return accumulator
+    }, [])
+
+    for (let index = 0; index < expectedCountry.length; index++) {
+      expect(names).toContain(expectedCountry[index])
+    }
   })
 })
